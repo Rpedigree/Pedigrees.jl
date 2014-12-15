e9ad3a78654e341b9e6097f1c555601f69334fe8
Douglas Bates <dmbates@gmail.com>
2014-12-15 16:42:01 -0600

    Remove the C code and deps directory (hurray!)

--- src/pedigree.jl
+++ src/pedigree.jl
@@ -1,11 +1,5 @@
 module pedigree
 
-## if isfile(joinpath(Pkg.dir("pedigree"),"deps","deps.jl"))
-##     include("../deps/deps.jl")
-## else
-##     error("pedigree not properly installed. Please run Pkg.build(\"pedigree\")")
-## end
-
     export Pedigree, laporder, Tinvt, Ltrans
 
     include("types.jl")
